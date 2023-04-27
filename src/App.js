c684bddfa40ac6678d328f7078c5465c895e97bd
Cong Le <congldqn888@gmail.com>
2023-04-27 22:37:13 +0700

    install tailwindcss into the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">App</div>;
+  const [work, setWork] = useState("");
+
+  return (
+    <div className="flex gap-8 h-screen justify-center border border-red-500 items-center">
+      <input
+        type="text"
+        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+        value={work}
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
+    </div>
+  );
 }
 
 export default App;
